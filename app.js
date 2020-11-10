d24bd2705afcccaf8b285abfd262252105d89b0d
Sam <sammymenaged@yahoo.com>
2020-11-10 10:55:20 -0500

    made passwords less secure lol

--- app.js
+++ app.js
@@ -7,11 +7,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const session = require('express-session');
 const passport = require("passport");
 const passportLocalMongoose = require("passport-local-mongoose");
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const FacebookStrategy = require('passport-facebook').Strategy;
 const findOrCreate = require("mongoose-findorcreate");
 
 const app = express();
@@ -19,12 +16,6 @@ const app = express();
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
-
-app.use(session({
-  secret: 'super duper secret',
-  resave: false,
-  saveUninitialized: true,
-}));
 
 app.use(passport.initialize());
 app.use(passport.session());
@@ -56,19 +47,6 @@ const User = new mongoose.model("User", userSchema);
 
 ////////////////////////////////// AUTHENITCATION ///////////////////////////////////////
 
-passport.use(User.createStrategy());
-
-passport.serializeUser(function(user, done) {
-  done(null, user.id);
-});
-
-passport.deserializeUser(function(id, done) {
-  User.findById(id, function(err, user) {
-    done(err, user);
-  });
-});
-
-
 
 
 ////////////////////////////////// HOME PAGE ///////////////////////////////////////
@@ -76,28 +54,6 @@ passport.deserializeUser(function(id, done) {
 app.get("/", function(req, res) {
   res.render("home");
 });
-
-////////////////////////////////// GOOGLE USERS ///////////////////////////////////////
-
-app.get("/auth/google",
-  passport.authenticate('google', { scope: ["profile"] })
-);
-
-app.get("/auth/google/secrets",
-  passport.authenticate("google", { failureRedirect: '/login' }),
-  function(req, res) {
-    // Successful authentication, redirect home.
-    res.redirect('/secrets');
-  });
-
-app.get('/auth/facebook', passport.authenticate('facebook'));
-
-app.get('/auth/facebook/secrets',
-  passport.authenticate("facebook", { failureRedirect: '/login' }),
-  function(req, res) {
-    // Successful authentication, redirect home.
-    res.redirect('/secrets');
-  });
 
 
 ////////////////////////////////// LOGIN ///////////////////////////////////////
@@ -108,39 +64,19 @@ app.get("/login", function(req, res) {
 
 app.post("/login", function(req,res) {
 
-  User.register({username: req.body.username}, req.body.password, function(err, user) {
-    if (err) {
-      console.log(err);
-      res.redirect("/login");
-    } else {
-      passport.authenticate("local")(req, res, function() {
-        res.redirect("/secrets");
-      });
-    }
+  const user = new User({
+    username: req.body.username,
+    password: req.body.password
   });
+
+  user.save();
+  res.redirect("/secrets");
 
 });
 
 ////////////////////////////////// REGISTER ///////////////////////////////////////
 
-app.get("/register", function(req, res) {
-  res.render("register");
-});
 
-app.post("/register", function(req,res) {
-
-  User.register({username: req.body.username}, req.body.password, function(err, user) {
-    if (err) {
-      console.log(err);
-      res.redirect("/register");
-    } else {
-      passport.authenticate("local")(req, res, function() {
-        res.redirect("/secrets");
-      });
-    }
-  });
-
-});
 
 ////////////////////////////////// SECRETS PAGE ///////////////////////////////////////
 
@@ -156,34 +92,6 @@ app.get("/secrets", function(req,res) {
   });
 });
 
-////////////////////////////////// SUBMIT PAGE ///////////////////////////////////////
-
-app.get("/submit", function(req, res) {
-  if (req.isAuthenticated()) {
-    res.render("submit");
-  } else {
-    res.redirect("/login");
-  }
-});
-
-app.post("/submit", function(req, res) {
-  const submittedSecret = req.body.secret;
-
-  console.log(req.user);
-
-  User.findById(req.user.id, function(err, foundUser) {
-    if (err) {
-      console.log(err);
-    } else {
-      if (foundUser) {
-        foundUser.secret = submittedSecret;
-        foundUser.save(function() {
-          res.redirect("/secrets");
-        });
-      }
-    }
-  });
-});
 
 ////////////////////////////////// LOGOUT ///////////////////////////////////////
 
